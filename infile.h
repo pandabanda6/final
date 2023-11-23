6f907446fecb96cce452db1800fc007ad3183cd7
pandabanda6 <128661388+pandabanda6@users.noreply.github.com>
2023-11-22 17:37:26 -0800

    Create infile.h

---
+++ infile.h
@@ -0,0 +1,36 @@
+#include <fstream>
+#include <string>
+#include "CandidateList.h"
+void createCandidateList(ifstream& infile, CandidateList& candidateList)
+{
+int ssn = 0;
+int allVotes[NUM_OF_CAMPUSES];
+string fName, lName;
+infile >> ssn;
+while (ssn != -999)
+{
+CandidateType newCandidate;
+infile >> fName;
+infile >> lName;
+newCandidate.setPersonInfo(fName, lName, ssn);
+for (int i = 0; i < NUM_OF_CAMPUSES; ++i)
+{
+infile >> allVotes[i];
+newCandidate.updateVotesByCampus(i, allVotes[i]);
+}
+candidateList.addCandidate(newCandidate);
+infile >> ssn;
+}
+}
+void readCandidateData(CandidateList& candidateList)
+{
+ifstream infile;
+infile.open("candidate_data.txt");
+if (!infile)
+{
+cerr << "Input file does not exist." << endl;
+exit(1);
+}
+createCandidateList(infile, candidateList);
+infile.close();
+}
