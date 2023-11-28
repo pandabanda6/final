629f51b929a11e095b65ca4ab8529443a7b29373
pandabanda6 <128661388+pandabanda6@users.noreply.github.com>
2023-11-27 20:48:27 -0800

    Update PersonType.cpp

--- PersonType.cpp
+++ PersonType.cpp
@@ -1,35 +1,50 @@
 #include "PersonType.h"
 #include <iostream>
+#include <string>
 
-PersonType::PersonType() {
+PersonType::PersonType() 
+{
     SSN = 0; // Default constructor initializes SSN to 0
 }
 
-PersonType::PersonType(std::string newfName, std::string newlName, int newSSN) {
+PersonType::PersonType(std::string newfName, std::string newlName, int newSSN) 
+{
     fName = newfName;
     lName = newlName;
     SSN = newSSN;
 }
 
-std::string PersonType::getFirstName() const {
+void PersonType::setPersonInfo(std::string newfName, std::string newlName, int newSSN) 
+{
+    fName = newfName;
+    lName = newlName;
+    SSN = newSSN;
+}
+
+std::string PersonType::getFirstName() const 
+{
     return fName;
 }
 
-std::string PersonType::getLastName() const {
+std::string PersonType::getLastName() const 
+{
     return lName;
 }
 
-int PersonType::getSSN() const {
+int PersonType::getSSN() const 
+{
     return SSN;
 }
 
-void PersonType::printName() const {
+void PersonType::printName() const 
+{
     std::cout << lName << ", " << fName << std::endl;
 }
 
-void PersonType::printPersonInfo() const {
+void PersonType::printPersonInfo() const 
+{
     printSSN();
-    std::cout << fName << " " << lName << std::endl;
+    std::cout << " " << fName << " " << lName << std::endl;
 }
 
 void PersonType::printSSN() const {
@@ -38,11 +53,14 @@ void PersonType::printSSN() const {
     ssn %= 1000000;
     int part2 = ssn / 10000;
     ssn %= 10000;
-    int part3 = ssn;
-    // Format the dashes
-    std::cout << part1 << "-" << part2 << "-" << part3 << std::endl;
+    int part3 = ssn / 1;
+    ssn %= 1;
+    //Format the dashes
+
+    std::cout << part1 << "-" << part2 << "-" << part3;
 }
 
 PersonType::~PersonType() {
     // Destructor code, if needed
 }
+
