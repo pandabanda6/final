2113dc13234544d84dd7314e8efd865ebc950b6c
pandabanda6 <128661388+pandabanda6@users.noreply.github.com>
2023-11-27 20:34:02 -0800

    Update CandidateList.cpp

--- CandidateList.cpp
+++ CandidateList.cpp
@@ -1,31 +1,28 @@
 #include "CandidateList.h"
-#include "PersonType.h"
-#include "Node.h"
 
 
-CandidateList::CandidateList()
+
+CandidateList :: CandidateList()
 {
-    first = nullptr;
-    last = nullptr;
-    count = 0;
+	first = nullptr;
+	last = nullptr;
+	count = 0;
 }
+void CandidateList::addCandidate(const CandidateType& candidate)
+ {
+     Node* newNode = new Node(candidate, nullptr);
 
-void CandidateList::addCandidate(const CandidateType& candidate) 
-{
-    Node* newNode = new Node(candidate, nullptr);
+     if (count == 0) {
+         first = newNode;
+         last = newNode;
+     }
+     else {
+         last->setLink(newNode);
+         last = newNode;
+     }
 
-    if (count == 0) {
-        first = newNode;
-        last = newNode;
-    }
-    else {
-        last->setLink(newNode);
-        last = newNode;
-    }
-
-    count++;
-}
-
+     count++;
+ }
 int CandidateList::getWinner(CandidateType candidate) // come back to check the if else later
 {
 
@@ -40,10 +37,10 @@ int CandidateList::getWinner(CandidateType candidate) // come back to check the 
     int winnerSSN = first->getCandidate().getSSN();
 
     Node* current = first->getLink();
-    while (current)
+    while (current) 
     {
         int currentVotes = current->getCandidate().getTotalVotes();
-        if (currentVotes > highestVotes)
+        if (currentVotes > highestVotes) 
         {
             highestVotes = currentVotes;
             winnerSSN = current->getCandidate().getSSN();
@@ -53,21 +50,6 @@ int CandidateList::getWinner(CandidateType candidate) // come back to check the 
 
     return winnerSSN;
 }
-
-  
-
-#include "CandidateList.h"
-#include "PersonType.h"
-#include "Node.h"
-
-CandidateList::CandidateList()
-{
-    first = nullptr;
-    last = nullptr;
-    count = 0;
-}
-
-// ... other functions ...
 
 bool CandidateList::searchCandidate(int SSN)
 {
@@ -86,10 +68,10 @@ bool CandidateList::searchCandidate(int SSN)
             // Candidate with the specified SSN found
             cout << "Candidate found:\n";
             cout << "SSN: " << current->getCandidate().getSSN() << "\n";
-            cout << "Name: " << current->getCandidate().getName() << "\n";
+            //cout << "Name: " << current->getCandidate().printName() << "\n";
             return true; // Candidate found, return true
         }
-
+       
         current = current->getLink();
     }
 
@@ -98,45 +80,97 @@ bool CandidateList::searchCandidate(int SSN)
     return false;
 }
 
+void CandidateList :: printCandidateName(int SSN)
+{
+    if (count == 0)
+    {
+        cout << "=> List is empty\n";
+        
+    }
 
+    Node* current = first;
 
-void CandidateList::printCandidateName(int SSN)
+    while (current)
+    {
+        if (current->getCandidate().getSSN() == SSN)
+        {
+            // Candidate with the specified SSN found
+            cout << "Candidate found:\n";
+            cout << "Name: ";
+            current->getCandidate().printName();
+       
+        }
+
+        current = current->getLink();
+    }
+
+    // Candidate not found
+    cout << "=> SSN not in the list\n";
+    
+}
+void CandidateList :: printAllCandidate()
+{
+    if (count == 0)
+    {
+        cout << "=> List is empty\n";
+       
+    }
+}
+void  CandidateList ::printCandidateCampusVotes(int SSN) 
+{
+
+    if (count == 0)
+    {
+        cout << "=> List is empty\n";
+
+    }
+
+    Node* current = first;
+
+    while (current)
+    {
+        if (current->getCandidate().getSSN() == SSN)
+        {
+            // Candidate with the specified SSN found
+            cout << "Candidate found:\n";
+            cout << "SSN: " << current->getCandidate().getVotesByCampus(0) << "\n";
+            //cout << "Name: " << current->getCandidate().printName() << "\n";
+           
+        }
+
+        current = current->getLink();
+    }
+
+}
+void CandidateList ::printCandidateTotalVotes(int SSN)
 {
     if (count == 0)
     {
         cout << "=> List is empty\n";
 
     }
-}
-void CandidateList::printAllCandidate()
-{
-    if (count == 0)
-    {
-        cout << "=> List is empty\n";
 
+    Node* current = first;
+
+    while (current)
+    {
+        if (current->getCandidate().getSSN() == SSN)
+        {
+            // Candidate with the specified SSN found
+            cout << "Candidate found:\n";
+            cout << "SSN: " << current->getCandidate().getTotalVotes() << "\n";
+            //cout << "Name: " << current->getCandidate().printName() << "\n";
+
+        }
+
+        current = current->getLink();
     }
 }
-void  CandidateList::printCandidateCampusVotes()
-{
-    if (count == 0)
-    {
-        cout << "=> List is empty\n";
-
-    }
-}
-void CandidateList::printCandidateTotalVotes()
-{
-    if (count == 0)
-    {
-        cout << "=> List is empty\n";
-
-    }
-}
-void CandidateList::desrtoyList()
+void CandidateList ::desrtoyList()
 {
 
 }
 CandidateList :: ~CandidateList()
 {
-
+    desrtoyList();
 }
